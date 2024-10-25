2b54d913fcd057dd32a6336d4b7b79e5415d88c2
Klaas Reineke <klaas.reineke@outlook.com>
2024-10-25 22:21:07 +0200

    Add publish to steam command via bash

---
+++ build/publish_workshop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# get login parameter from other file as variables
+source $(dirname "$0")/../steamcredentials.sh
+
+sdk_dir=/Users/klaas/JupiterHell_mods/steamworks_sdk_160
+
+# see the manual for publishing
+# https://partner.steamgames.com/doc/features/workshop/implementation?l=english#SteamCmd
+$sdk_dir/tools/ContentBuilder/builder_osx/steamcmd.sh +login $login $password +workshop_build_item /Users/klaas/JupiterHell_mods/EnemyAlerter/steamworkshop_ea.vdf +quit
